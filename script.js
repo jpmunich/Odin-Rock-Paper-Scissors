028dd4dd1e22f4ce018b00235098839878ca0cb0
jpmunich <parkercoyle8@gmail.com>
2023-07-07 19:25:37 -0400

    Basic logic for game

---
+++ script.js
@@ -0,0 +1,25 @@
+function getComputerTurn() {
+    return Math.floor(Math.random() * 3);
+}
+
+function getPlayerTurn() {
+    return Math.floor(Math.random() * 3);
+}
+
+function playRound(computer, player) {
+    const playerWinConditions = [[2, 0], [0, 1], [1, 2]];
+    const computerWinConditions = [[0, 2], [1, 0], [2, 1]];
+    const result = [computer, player];
+
+    for (let i = 0; i < 3; i++) {
+        if (playerWinConditions[i].join("") === result.join("")) return "player";
+        else if (computerWinConditions[i].join("") === result.join("")) return "computer";
+    }
+        return "tie";
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        return playRound(getComputerTurn(), 1);
+    }
+}
