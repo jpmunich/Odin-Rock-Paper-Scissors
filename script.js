672aaf8b946afd35f5496eee9ad12718a0be73e0
jpmunich <parkercoyle8@gmail.com>
2023-07-08 01:49:52 -0400

    Clicking buttons now updates score

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const weapons = document.querySelectorAll(".weapon-img");
+const score = Array.from(document.querySelectorAll(".score-label"));
+
+let computerScore = 0;
+let playerScore = 0;
+
+score[0].innerText = "Player: " + playerScore;
+score[1].innerText = "Computer: " + computerScore;
 
 for (let i = 0; i < weapons.length; i++) {
     weapons[i].addEventListener("click", () => {
         playRound(getComputerTurn(), i)
+        console.log(playerScore)
     })
 }
 
@@ -20,14 +28,10 @@ function playRound(computer, player) {
     const result = [computer, player];
 
     for (let i = 0; i < 3; i++) {
-        if (playerWinConditions[i].join("") === result.join("")) return "player";
-        else if (computerWinConditions[i].join("") === result.join("")) return "computer";
+        if (playerWinConditions[i].join("") === result.join("")) playerScore++;
+        else if (computerWinConditions[i].join("") === result.join("")) computerScore++;
     }
-        return "tie";
-}
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        return playRound(getComputerTurn(), 1);
-    }
+    score[0].innerText = "Player: " + playerScore;
+    score[1].innerText = "Computer: " + computerScore;
 }
