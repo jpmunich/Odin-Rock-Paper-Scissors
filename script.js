64984ac393c44b3eec4dc35db97b889fe63f92d3
jpmunich <parkercoyle8@gmail.com>
2023-07-08 02:47:23 -0400

    Game ends then resets once score reaches 5

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const gameInfo = document.querySelector('h2');
 const gameInfoSubheading = document.querySelector('#game-info-subheading');
 const playerSelectedWeapon = document.querySelector("#player-selected-weapon");
 const computerSelectedWeapon = document.querySelector('#computer-selected-weapon');
+const endGameButton = document.querySelector('#button');
+const endGameScreen = document.querySelector('.end-game-screen');
+const overlay = document.querySelector('.overlay');
 
 let computerScore = 0;
 let playerScore = 0;
@@ -18,15 +21,37 @@ for (let i = 0; i < weapons.length; i++) {
     })
 }
 
+endGameButton.addEventListener("click", resetGame);
+
 weapons[0].addEventListener("click", () => {playerSelectedWeapon.src = "./images/rock.png"});
 weapons[1].addEventListener("click", () => {playerSelectedWeapon.src = "./images/paper.png"});
 weapons[2].addEventListener("click", () => {playerSelectedWeapon.src = "./images/scissors.png"});
 
-const updateScore = () => {
+const updateGame = () => {
     score[0].innerText = "Player: " + playerScore;
     score[1].innerText = "Computer: " + computerScore;
+
+    if (computerScore > 4 || playerScore > 4) {
+        endGame();
+    }
 }
-updateScore();
+updateGame();
+
+function endGame() {
+    computerScore = 0;
+    playerScore = 0;
+    overlay.setAttribute("id", "overlay");
+    endGameScreen.setAttribute("id", "game-over");
+    updateGame();
+}
+
+function resetGame() {
+    gameInfo.innerText = "Choose Your Weapon!";
+    overlay.setAttribute("id", "disappear");
+    endGameScreen.setAttribute("id", "disappear");
+    playerSelectedWeapon.src = "./images/question-mark.png";
+    computerSelectedWeapon.src = "./images/question-mark.png";
+}
 
 const getComputerTurn = () => {return Math.floor(Math.random() * 3);}
 
@@ -49,5 +74,5 @@ function playRound(computer, player) {
         }
 
     }
-    updateScore();
+    updateGame();
 }
