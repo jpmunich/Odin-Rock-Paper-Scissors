becce8b621a9251a8794339d944ce31862006ff8
jpmunich <parkercoyle8@gmail.com>
2023-07-08 02:04:16 -0400

    Condensed logic + Created nodes for gameInfo

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 const weapons = document.querySelectorAll(".weapon-img");
 const score = Array.from(document.querySelectorAll(".score-label"));
+const gameInfo = document.querySelector('h2');
+const gameInfoSubheading = document.querySelector('#game-info-subheading');
 
 let computerScore = 0;
 let playerScore = 0;
 
-score[0].innerText = "Player: " + playerScore;
-score[1].innerText = "Computer: " + computerScore;
-
 for (let i = 0; i < weapons.length; i++) {
     weapons[i].addEventListener("click", () => {
         playRound(getComputerTurn(), i)
-        console.log(playerScore)
     })
 }
 
-function getComputerTurn() {
-    return Math.floor(Math.random() * 3);
+const updateScore = () => {
+    score[0].innerText = "Player: " + playerScore;
+    score[1].innerText = "Computer: " + computerScore;
 }
+updateScore();
 
-function getPlayerTurn() {
-    return Math.floor(Math.random() * 3);
-}
+const getComputerTurn = () => {return Math.floor(Math.random() * 3);}
+const getPlayerTurn = () => {return Math.floor(Math.random() * 3);}
 
 function playRound(computer, player) {
     const playerWinConditions = [[2, 0], [0, 1], [1, 2]];
@@ -28,10 +27,12 @@ function playRound(computer, player) {
     const result = [computer, player];
 
     for (let i = 0; i < 3; i++) {
-        if (playerWinConditions[i].join("") === result.join("")) playerScore++;
+        if (playerWinConditions[i].join("") === result.join("")) {
+            playerScore++;
+
+
+        }
         else if (computerWinConditions[i].join("") === result.join("")) computerScore++;
     }
-
-    score[0].innerText = "Player: " + playerScore;
-    score[1].innerText = "Computer: " + computerScore;
+    updateScore();
 }
